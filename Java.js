71753c02063dc3b71dd52e8e709b261d6243394b
Steven <sjetho@me.com>
2020-02-04 13:40:06 -0600

    V1

---
+++ Java.js
@@ -0,0 +1,62 @@
+
+// console.log('hello')
+
+// var mystring = "";
+// var myInt = 2.1;
+// var myBool = true;
+// var myString = "hello";
+
+// console.log(myString.length)
+
+// var age = 20
+
+// if (age >= 21) {
+//     console.log("You get free beer");
+//   } else if (age < 18) {
+//     console.log("What are you even doing here?");
+//   } else {
+//     console.log("Sorry no beer for you");
+//   }
+
+// var expr = "Papayas";
+//   if(expr == 'Oranges')
+//   {
+//       console.log('Oranges are $0.59 a pound.');
+//   }
+//   else if(expr == 'Mangoes')
+//   {
+//   }
+//   else if(expr == 'Papayas')
+//   {
+//       console.log('Mangoes and papayas are $2.79 a pound.');
+//       // expected output: "Mangoes and papayas are $2.79 a pound."
+//   }
+//   else
+//   {
+//       console.log('Sorry, we are out of ' + expr + '.');
+//   }
+
+// var count = 0;
+
+// while (count < 10){
+//     console.log(count);
+//     count++
+// }
+
+
+// for(var count = 0; count < 10; count ++){
+//     console.log(count)
+
+//     for(var inner = 0; inner < 3; inner++){
+//         console.log(inner);
+//     }
+// }
+
+var lottoNums = [];
+
+lottoNums.push(3)
+    console.log(lottoNums);
+
+lottoNums.push(34)
+    console.log(lottoNums);
+
